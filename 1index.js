df5999e577428a9ccbe9581b5202a0a1cd1547fa
Ayanami Rei <yahooberserker@gmail.com>
2025-10-22 15:29:07 +0300

    Add files via upload

---
+++ 1index.js
@@ -0,0 +1,14 @@
+/* убрать комент
+const company = prompt (" введите название компании в которой хотите работать")
+alert ("ifvhffer")
+console.log(company)
+*/
+
+// console.log("text" + "text" + "text")
+/* убрать комент
+// сразу есть пробел между словами
+const a = "денис";
+const b = "денисович";
+
+console.log(`${a} денисов ${b}`); // интерполяция
+*/
