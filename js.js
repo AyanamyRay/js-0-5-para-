bdbb656ea711d371fe56d79461b07ab4d22a975d
Ayanami Rei <yahooberserker@gmail.com>
2025-10-22 15:01:12 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+// конкатенация 
+/* (убрать комент)
+console.log("a" + "b" + "c"); // abc слитно
+
+console.log("Матвей" + " " + "Гвоздев" + " " + "16 лет");
+*/
+
+// переменные 
+/* (убрать комент)
+let a = "text"; // можем перезаписть
+a = "sdfsdfsdf"
+
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+
+const b = "text" // нельзя изменить 
+// b = "asdasddaasd" - будет ошибка
+
+let c = 4 + 5; // сначала выполняется +, а потом =, получаетя эта переменная ровна 9
+const d = 4 - 3;
+
+console.log(a + d); // 10
+*/
+// let firstSum = 4 + 5;
+// можно спросить у ии как назвать переменную 
+/*
+const firstName = "Матвей";
+const lastName = "Гвоздев";
+const age = "16 лет";
+
+console.log(firstName + " " + lastName + " " + age);
+*/
+/* промпт чисто в браузере 
+const firstName = prompt ("введите имя");
+const lastName = prompt ("введите фамилию");
+*/
+
