2af0bb7354cc1d9225ce1f2049b1323b19fe691f
Ayanami Rei <yahooberserker@gmail.com>
2025-10-22 13:27:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+console.log("hello world");
+
+console.log("hello");
+
+// комент
+
+ /*
+ sfsdfsfsfsfsf
+ sdfsfsf
+ sdfsdffs
+ */
+
+ /*
+ console.log("David");
+ console.log("Dima");
+ console.log("Nekit");
+*/
+
+/*
+console.log(4+4);
+console.log(5-4);
+console.log(5*6);
+console.log(10/2);
+
+console.log(5**2); //25 степень
+
+console.log(10%3); // 1 остаток от деления
+
+console.log(10%2) // 0
+*/
+/* 
+console.log(2 + 2 * 2); // 6
+console.log((2 + 2) * 2); // 8
+
+console.log(-5);
+*/
+console.log(3**5);
+console.log(-8 / -4);
+console.log(100%3);
+
+// console.log(3**5 + -8 / -4 + 100%3);
+
+let a = 3**5;
+let b = -8 / -4
+let c = 100%3
+
+console.log(a + b + c); 
+
