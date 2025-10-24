c10fdc2e63e26c4d9eecfbb604b12635e41f7aab
Ayanami Rei <yahooberserker@gmail.com>
2025-10-24 14:04:05 +0300

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,81 @@
+/*
+
+const a = "text";
+const b = "text";
+console.log(`${a} text ${b}`);
+
+*/
+
+// const str = "text"; // 4
+//         0123
+
+/*
+
+console.log(str[1]);
+
+console.log(`${str[0]} ${str[1]} ${str[2]} ${str[3]}`);
+
+*/
+
+/*
+
+const one = "Naharis";
+const two = "Mormont";
+const three = "sand";
+
+console.log(`${one[2]} ${two[1]} ${three[3]} ${two[4]} ${two[2]}`);
+
+*/
+
+// console.log(`${строка[символ]}`);
+
+
+/*
+
+console.log(1 + "7"); // 17 // string 
+
+console.log("1" + "7"); // 17
+
+console.log(2 * "7"); // 2 * 7 == 14
+
+console.log(1 * "str"); // NaN - Not a Number
+
+console.log("text" + "text1"); // NaN 
+
+*/
+
+/*
+
+let a = 0; // автоматияески опр тип  = number
+let str = "text"; //string
+
+a = "text1"; // поменялось на string
+str = 0; // string
+
+*/
+
+/*
+
+const s = prompt ("введите слово");
+const st = prompt ("введите слово");
+const str = prompt ("введите слово");
+
+console.log(`${s[0]} ${st[0]} ${str[0]}`)  (вписать слова а потом нажать F12)
+
+*/
+
+// let stre = "text";
+// console.log(str.length); // 4
+
+/*
+
+const s = prompt ("введите слово");
+const st = prompt ("введите слово");
+const str = prompt ("введите слово");
+
+console.log(`${s.length} ${st.length} ${str.length}`); // выводит последние символы как цифры в F12 от нуля
+alert(`${s[0]} ${st[0]} ${str[0]}`) // выводит первые символы в строке
+
+// alert(`${s.length[s.length-1]} ${st.length[st.length-1]} ${str.length[str.length-1]}`); // счет от нуля поэтому -1 и мы получим то число знаков которое есть в строке
+
+*/
